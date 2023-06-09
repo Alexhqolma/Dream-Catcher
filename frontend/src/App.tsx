5c5df90372232cd01d888dbc18204688894fc684
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-10 00:52:06 +0300

    add mock registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
-import './App.scss';
 import { useAppSelector } from './store/hooks';
 import { selectUser } from './store/features/user/userSlice';
+
+import './App.scss';
 
 export const App: React.FC = () => {
   const user = useAppSelector(selectUser);
 
   return (
     <div>
-      {user && <h1>{`Hello, ${user.name}!`}</h1>}
-
       <Header />
+      {user && <h1>{`Hello, ${user.name}!`}</h1>}
 
       <main>
         <Layout />
--- frontend/src/pages/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { RegistrationForm } from "../../components/registrationForm";
-import { useAppDispatch } from "../../store/hooks";
-import { postUser } from "../../store/sagas/sagaActions";
 
 const RegistrationPage: React.FC = () => {
   return (

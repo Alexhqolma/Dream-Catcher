2b043e13c9a78ccce6e11578fe665bf53c05b40f
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-11 01:38:03 +0300

    fix dreams container

--- frontend/src/components/dreamItem/DreamCard.tsx
+++ frontend/src/components/dreamItem/DreamCard.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Dream } from '../../types/Dream';
 
 import './DreamCard.scss';
-import { Link } from 'react-router-dom';
 
 interface DreamItemProps {
   dream: Dream;

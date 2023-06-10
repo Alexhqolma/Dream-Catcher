029ad45e8c4a25e5cff7769f203588430d0c43b9
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-11 02:32:48 +0300

    fix card

--- frontend/src/components/DreamCard/DreamCard.tsx
+++ frontend/src/components/DreamCard/DreamCard.tsx
@@ -11,12 +11,12 @@ interface DreamItemProps {
 export const DreamCard: React.FC<DreamItemProps> = ({ dream }) => {
   return (
     <div className="dream-card">
-      <h4 className="dream-card__title">{dream.title}</h4>
       <img
         className="dream-card__img" 
         src={dream?.photo || undefined} 
         alt="photo" 
       />
+      <h4 className="dream-card__title">{dream.title}</h4>
       <p className="dream-card__body">{dream.body}</p>
 
       <Link to={`/dream/${dream.id}`} className='dream-card__button' >Details</Link>

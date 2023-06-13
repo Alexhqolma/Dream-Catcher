dea471ba8fcffe1708b60d6f987231556f5f3b61
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-13 03:08:09 +0300

    fix slider

--- frontend/src/components/MainSwiper/MainSwiper.tsx
+++ frontend/src/components/MainSwiper/MainSwiper.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-import "./MainSwiper.scss";
 import { useAppSelector } from '../../store/hooks';
 import { selectMockData } from '../../mock/store/features/mock/mockSlice';
 import { Dream } from '../../types/Dream';
 
+
+import "./MainSwiper.scss";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -27,18 +26,26 @@ export const MainSwiper: React.FC = () => {
   const dreams = useAppSelector(selectMockData).slice(0, 4);
 
   return (
-    <Swiper
-      {...swiperSettings}
-      className="main-swiper"
-    >
-      {dreams.map(dream => (
-        <SwiperSlide
-          key={dream.id}
-        >
-          <MainSLide dream={dream} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        {...swiperSettings}
+        className="main-swiper"
+      >
+        {dreams.map(dream => (
+          <SwiperSlide
+            key={dream.id}
+          >
+            <MainSLide dream={dream} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swiper-pagination"></div>
+
+      <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
+
+      <div className="swiper-scrollbar"></div>
+    </>
   );
 };
 

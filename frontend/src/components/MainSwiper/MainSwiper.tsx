07c209a4757c17e68c053501390a3336c2d4f164
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-13 13:30:25 +0300

    add main slider

--- frontend/src/components/MainSwiper/MainSwiper.tsx
+++ frontend/src/components/MainSwiper/MainSwiper.tsx
@@ -1,58 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import { useAppSelector } from '../../store/hooks';
 import { selectMockData } from '../../mock/store/features/mock/mockSlice';
-import { Dream } from '../../types/Dream';
+import { MainSLide } from './MainSLide';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import "./MainSwiper.scss";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-const swiperSettings = {
-  spaceBetween: 0,
-  slidesPerView: 1,
-  autoplay: { 
-    delay: 400,
-    stopOnLastSlide: true,
-  },
-  onSlideChange: () => console.log('slide change'),
-  onSwiper: (swiper: any) => console.log(swiper),
-};
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
-};
-
-// export const MainSwiper: React.FC = () => {
-//   const dreams = useAppSelector(selectMockData).slice(0, 4);
-
-//   return (
-//     <>
-//       <Swiper
-//         {...swiperSettings}
-//         className="main-swiper"
-//       >
-//         {dreams.map(dream => (
-//           <SwiperSlide
-//             key={dream.id}
-//           >
-//             <MainSLide dream={dream} />
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </>
-//   );
-// };
-
 
 export const MainSwiper: React.FC = () => {
   const dreams = useAppSelector(selectMockData).slice(0, 4);
@@ -64,8 +19,8 @@ export const MainSwiper: React.FC = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     // adaptiveHeight: true,
-    pauseOnDotsHover: true,
-    autoplay: true,
+    // pauseOnDotsHover: true,
+    // autoplay: true,
     // customPaging: () => (
     //   <div
     //     className="slick-dot"
@@ -90,20 +45,3 @@ export const MainSwiper: React.FC = () => {
   );
 };
 
-interface MainSlideProps {
-  dream: Dream;
-}
-
-const MainSLide: React.FC<MainSlideProps> = ({ dream }) => {
-  useEffect(() => {
-    console.log('render slide');
-  }, [dream]);
-
-  return (
-    <div className="slider__banner">
-      {dream && (
-        <img className="slider__main-img" src={dream.photo || undefined} alt="" />
-      )}
-    </div>
-  )
-};

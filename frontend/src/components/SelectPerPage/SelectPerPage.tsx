385a19cbc7be9c9c4badb237ec1e0e496896ea49
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-15 23:51:46 +0300

    fix dreams container controls

---
+++ frontend/src/components/SelectPerPage/SelectPerPage.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEventHandler } from "react";
+import classNames from "classnames";
+
+import './SelectPerPage.scss';
+
+interface SelectPerPageProps {
+  onChange?: ChangeEventHandler<HTMLSelectElement>;
+  className?: string;
+  values: number[];
+  countAllDreams?: number;
+  defaultValue?: number;
+}
+
+export const SelectPerPage: React.FC<SelectPerPageProps> = ({ 
+  countAllDreams: countDreams,
+  onChange = () => console.warn('no OnClick function'),
+  className,
+  values,
+  defaultValue,
+ }) => {
+
+  return (
+    <label 
+      htmlFor="DreamsPerPage" 
+      className={classNames('SelectPerPage', className)}
+    >
+      Dreams per page&nbsp;
+      <select
+        value={defaultValue}
+        className="DreamsPerPage__select"
+        name="DreamsPerPage"
+        id="DreamsPerPage"
+        onChange={onChange}
+      >
+        {values.map(value => (
+          <option value={String(value)}>{value}</option>
+        ))}
+        {countDreams && (
+          <option value={String(countDreams)}>all</option>
+        )}
+      </select>
+    </label>
+  )
+}

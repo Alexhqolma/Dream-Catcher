6759a266aa483f345bd26b776bca196ac375b9b6
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-17 01:11:47 +0300

    fix custom select

---
+++ frontend/src/components/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import arrow from './../../assets/images/arrow-down.svg';
+
+import './CustomSelect.scss';
+
+interface CustomSelectProps {
+  onChange: (value: string) => void;
+  className?: string;
+  values: string[];
+  valueForAll?: string;
+  currentValue: string;
+}
+
+export const CustomSelect: React.FC<CustomSelectProps> = ({ 
+  valueForAll,
+  onChange = () => console.warn('no OnClick function'),
+  className,
+  values,
+  currentValue,
+ }) => {
+  const [isOpenSelect, setIsOpenSelect] = useState(false);
+
+  const toggleSelect  = () => {
+    console.log('isOpenSelect = ', isOpenSelect);
+    setIsOpenSelect(isOpenSelect => !isOpenSelect);
+  };
+
+  const valueHandler = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    e.stopPropagation();
+    console.log(e.currentTarget.dataset.value);
+    onChange(e.currentTarget.dataset.value || 'no dataset.value');
+    setTimeout(() => {
+      toggleSelect();
+    }, 200)
+  };
+
+  console.log(currentValue, valueForAll);
+
+  return (
+    <div
+      className={classNames('CustomSelect', className)}
+      onClick={toggleSelect}
+    > 
+      <p className="CustomSelect__label">Dreams per page</p>
+
+      <div className="CustomSelect__select-container">
+        <ul
+          className="CustomSelect__select"
+          id="DreamsPerPage"
+        >
+          {!isOpenSelect && (
+            <li
+              data-value={currentValue}
+              onClick={e => valueHandler(e)}
+              className="CustomSelect__option"
+            >
+              {currentValue === valueForAll ? 'all' : currentValue}
+            </li>
+          )}
+
+          {isOpenSelect && values.map(value => (
+            <li
+              key={value}
+              data-value={value}
+              onClick={e => valueHandler(e)}
+              className="CustomSelect__option"
+            >
+              {value}
+            </li>
+          ))}
+
+          {isOpenSelect && valueForAll && (
+            <li 
+              data-value={String(valueForAll)}
+              onClick={e => valueHandler(e)}
+              className="CustomSelect__option"
+            >
+              all
+            </li>
+          )}
+        </ul>
+      </div>
+
+      <img className="CustomSelect__select-arrow" src={arrow} alt="arrow" />
+    </div>
+  )
+}
--- frontend/src/components/DreamsContainer/DreamsContainer.tsx
+++ frontend/src/components/DreamsContainer/DreamsContainer.tsx
@@ -4,13 +4,13 @@ import { selectMockData } from "../../mock/store/features/mock/mockSlice";
 import { DreamCard } from "../DreamCard/DreamCard";
 import { Dream } from "../../types/Dream";
 import { BasicPagination } from "../BasicPagination";
-import { SelectPerPage } from "../SelectPerPage";
 
 import './DreamsContainer.scss';
+import { CustomSelect } from '../CustomSelect';
 
 export const DreamsContainer: React.FC = () => {
   const dreams = useAppSelector(selectMockData);
-  const [dreamsPerPage, setDreamsPerPage] = useState(8);
+  const [dreamsPerPage, setDreamsPerPage] = useState<number>(8);
   const [page, setPage] = useState(1);
 
   const onPageChange = (page: number) => {
@@ -24,14 +24,16 @@ export const DreamsContainer: React.FC = () => {
   const totalPages = Math.ceil(dreams.length / dreamsPerPage);
 
   return (
-    <div className="DreamsContainer">
+    <div 
+      className="DreamsContainer" 
+    >
       <div className="DreamsContainer__controls">
-        <SelectPerPage
-          onChange={(e) => setDreamsPerPage(+e.target.value)}
+        <CustomSelect
+          onChange={(value) => setDreamsPerPage(+value)}
           className="DreamsContainer__select"
-          values={[4, 8, 16, 48]}
-          countAllDreams={dreams.length}
-          defaultValue={dreamsPerPage}
+          values={['4', '8', '16', '48']}
+          valueForAll={dreams.length.toString()}
+          currentValue={dreamsPerPage.toString()}
         />
 
         <BasicPagination 

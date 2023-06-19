0dfab0092849a7628173c49e8ddf067a9eee4a5c
Alexhqolma <hqolma@gmail.com>
2023-06-19 14:37:26 +0300

    Cors correction 3

--- api/src/main/java/com/dreamcatcher/config/WebSecurityConfig.java
+++ api/src/main/java/com/dreamcatcher/config/WebSecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -24,8 +29,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .cors()
+                .csrf().disable().cors().configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeHttpRequests()
                 //.requestMatchers("/", "/inject", "/registration", "/swagger-ui").permitAll()
@@ -39,6 +43,19 @@ public class WebSecurityConfig {
                 .formLogin()
                 .and()
                 .build();
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        final var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     private static final String[] AUTH_WHITELIST = {

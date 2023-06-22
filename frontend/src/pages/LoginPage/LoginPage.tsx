bdc5ea863b6c21d41e4f44c5e92e31c90db9a37c
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-06-22 14:53:47 +0200

    login form created

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { LoginForm } from '../../components/LoginForm';
 
 const LoginPage: React.FC = () => {
   return (
     <main className="LoginPage">
       <h1 className='LoginPage__title title'>LoginPage</h1>
-
+      <LoginForm />
     </main>
   );
 }

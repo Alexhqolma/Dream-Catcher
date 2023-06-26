a43f1bb08fd63c5dd2c27f635a1a335a12d45012
Alexhqolma <hqolma@gmail.com>
2023-06-26 13:28:55 +0300

    Fixed Auth

--- api/src/main/java/com/dreamcatcher/controller/AuthenticationController.java
+++ api/src/main/java/com/dreamcatcher/controller/AuthenticationController.java
@@ -6,8 +6,10 @@ import com.dreamcatcher.security.AuthenticationResponse;
 import com.dreamcatcher.security.AuthenticationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
